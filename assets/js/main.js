f0577db8c53bb53038e721d2d120fe92dec846db
Alexander Kazeev <90039242+iamstew@users.noreply.github.com>
2022-08-25 23:35:30 +0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,29 @@
-var x = 0;
-let cart_open = document.querySelector('.shopping-bag');
-let cart = document.querySelector('.cart');
-let cart_close = document.querySelector('.cart__close');
+var cart = [];
 
-cart_close.addEventListener('click', function(){
-	cart.classList.remove('active');
-	document.querySelector('.main__wrapper').style.display = 'block'
-	setTimeout(function(){cart.style.display = 'none'}, 500);
-});
-
-cart_open.addEventListener('click', function(){
-	cart.style.display = 'flex';
-	document.querySelector('.main__wrapper').style.display = 'none'
-	setTimeout(function(){cart.classList.add('active')},10);
-});
+function cartToggle(){
+	if (document.querySelector('.cart').classList.contains('active')) {
+		document.querySelector('.cart').classList.remove('active');
+		setTimeout(function(){document.querySelector('.cart').style.display = 'none'}, 500);
+	}else{
+		document.querySelector('.cart').style.display = 'flex';
+		setTimeout(function(){document.querySelector('.cart').classList.add('active')},10);
+	}
+};
 
 window.addEventListener('scroll', function(){
-	// if (cart.classList.contains('active')==false){
-	// console.log(document.querySelector('.main__wrapper .title__block').getBoundingClientRect())
 	if (window.scrollY >= document.querySelector('.library__image').offsetHeight){
-		// document.querySelector('header').style.background = '#232324';
 		document.querySelector('.scrollTop').style.display = 'block';
 	}else{
-		// document.querySelector('header').style.background = 'initial';
 		document.querySelector('.scrollTop').style.display = 'none';
-	// }
 	}
-})
+});
 
 document.querySelector('.scrollTop').onclick = () => {
 	window.scrollTo(pageYOffset, 0);
-}
+};
+
+document.onclick = event => {
+	if (event.target.classList.contains('add-in-cart')){
+		console.log(event.target.closest(''))
+	}
+};
